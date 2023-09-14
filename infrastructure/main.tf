164dfdc6018fececf325744fdce3066546d91a0c
Peter Lehmann <peter@lehmann.zone>
2023-09-14 10:45:38 +0200

    Revert "Switch to ubuntu"
    This reverts commit 40d59e07b73a723660b0cf6253a5aaf902fb55b6.

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -27,7 +27,7 @@ resource "hcloud_server" "infra_xnee_de" {
   delete_protection  = false
   rebuild_protection = false
 
-  image     = "ubuntu-22.04"
+  image     = "debian-12"
   ssh_keys  = ["infra"]
 }
 

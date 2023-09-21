aba863c4f19d4af8c310bd8948b65266ff21d9a1
Peter Lehmann <peter@lehmann.zone>
2023-09-21 21:05:38 +0200

    Uncomment docker and traefik

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -15,10 +15,10 @@ terraform {
   }
 }
 
-# provider "docker" {
-#   host     = "ssh://root@infra.xnee.de:22"
-#   ssh_opts = ["-o", "StrictHostKeyChecking=no", "-o", "UserKnownHostsFile=/dev/null"]
-# }
+provider "docker" {
+  host     = "ssh://root@infra.xnee.de:22"
+  ssh_opts = ["-o", "StrictHostKeyChecking=no", "-o", "UserKnownHostsFile=/dev/null"]
+}
 
 resource "hcloud_ssh_key" "infra" {
   name       = "infra"
@@ -51,61 +51,61 @@ resource "hetznerdns_record" "infra_xnee_de_v6" {
   type    = "AAAA"
 }
 
-# resource "docker_image" "traefik" {
-#   name = "traefik:v3.0"
-# }
+resource "docker_image" "traefik" {
+  name = "traefik:v3.0"
+}
 
-# resource "docker_volume" "traefik" {
-#   name = "traefik_acme"
-# }
+resource "docker_volume" "traefik" {
+  name = "traefik_acme"
+}
 
-# resource "docker_container" "traefik" {
-#   name  = "traefik"
-#   image = docker_image.traefik.image_id
-#   mounts {
-#     target    = "/var/run/docker.sock"
-#     type      = "bind"
-#     source    = "/var/run/docker.sock"
-#     read_only = true
-#   }
+resource "docker_container" "traefik" {
+  name  = "traefik"
+  image = docker_image.traefik.image_id
+  mounts {
+    target    = "/var/run/docker.sock"
+    type      = "bind"
+    source    = "/var/run/docker.sock"
+    read_only = true
+  }
 
-#   volumes {
-#     container_path = "/acme"
-#     volume_name    = "traefik_acme"
-#   }
+  volumes {
+    container_path = "/acme"
+    volume_name    = "traefik_acme"
+  }
 
-#   ports {
-#     internal = 80
-#   }
-#   ports {
-#     internal = 443
-#   }
+  ports {
+    internal = 80
+  }
+  ports {
+    internal = 443
+  }
 
-#   command = [
-#     "--entrypoints.web.address=:80",
-#     "--entrypoints.websecure.address=:443",
-#     "--providers.docker",
-#     "--log.level=WARN",
-#     "--certificatesresolvers.leresolver.acme.httpchallenge=true",
-#     "--certificatesresolvers.leresolver.acme.email=peter@lehmann.zone",
-#     "--certificatesresolvers.leresolver.acme.storage=/acme/acme.json",
-#     "--certificatesresolvers.leresolver.acme.httpchallenge.entrypoint=web"
-#   ]
+  command = [
+    "--entrypoints.web.address=:80",
+    "--entrypoints.websecure.address=:443",
+    "--providers.docker",
+    "--log.level=WARN",
+    "--certificatesresolvers.leresolver.acme.httpchallenge=true",
+    "--certificatesresolvers.leresolver.acme.email=peter@lehmann.zone",
+    "--certificatesresolvers.leresolver.acme.storage=/acme/acme.json",
+    "--certificatesresolvers.leresolver.acme.httpchallenge.entrypoint=web"
+  ]
 
-#   labels {
-#     label = "traefik.http.routers.http-catchall.rule"
-#     value = "hostregexp(`{host:.+}`)"
-#   }
-#   labels {
-#     label = "traefik.http.routers.http-catchall.entrypoints"
-#     value = "web"
-#   }
-#   labels {
-#     label = "traefik.http.routers.http-catchall.middlewares"
-#     value = "redirect-to-https"
-#   }
-#   labels {
-#     label = "traefik.http.middlewares.redirect-to-https.redirectscheme.scheme"
-#     value = "https"
-#   }
-# }
+  labels {
+    label = "traefik.http.routers.http-catchall.rule"
+    value = "hostregexp(`{host:.+}`)"
+  }
+  labels {
+    label = "traefik.http.routers.http-catchall.entrypoints"
+    value = "web"
+  }
+  labels {
+    label = "traefik.http.routers.http-catchall.middlewares"
+    value = "redirect-to-https"
+  }
+  labels {
+    label = "traefik.http.middlewares.redirect-to-https.redirectscheme.scheme"
+    value = "https"
+  }
+}

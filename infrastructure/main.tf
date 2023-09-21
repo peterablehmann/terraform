bab45a7bf49eedb8c744837e6f997260c16b5fbf
Peter Lehmann <peter@lehmann.zone>
2023-09-21 20:41:22 +0200

    Create traefik container

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -15,6 +15,11 @@ terraform {
   }
 }
 
+provider "docker" {
+  host     = "ssh://root@infra.xnee.de:22"
+  ssh_opts = ["-o", "StrictHostKeyChecking=no", "-o", "UserKnownHostsFile=/dev/null"]
+}
+
 resource "hcloud_ssh_key" "infra" {
   name       = "infra"
   public_key = file("ssh/ed25519_infra.pub")
@@ -44,4 +49,67 @@ resource "hetznerdns_record" "infra_xnee_de_v6" {
   name    = "infra"
   value   = hcloud_server.infra_xnee_de.ipv6_address
   type    = "AAAA"
+}
+
+resource "docker_image" "traefik" {
+  name = "traefik:v3.0"
+}
+
+resource "docker_volume" "traefik" {
+  name = "traefik_acme"
+}
+
+resource "docker_container" "traefik" {
+  name  = "traefik"
+  image = docker_image.traefik.image_id
+  mounts = [
+    {
+      target    = "/var/run/docker.sock"
+      type      = "bind"
+      source    = "/var/run/docker.sock"
+      read_only = true
+    }
+  ]
+  volumes = [
+    {
+      container_path = "/acme"
+      volume_name    = "traefik_acme"
+    }
+  ]
+  ports = [
+    {
+      internal = 80
+    },
+    {
+      internal = 443
+    }
+  ]
+  command = [
+    "--entrypoints.web.address=:80",
+    "--entrypoints.websecure.address=:443",
+    "--providers.docker",
+    "--log.level=WARN",
+    "--certificatesresolvers.leresolver.acme.httpchallenge=true",
+    "--certificatesresolvers.leresolver.acme.email=peter@lehmann.zone",
+    "--certificatesresolvers.leresolver.acme.storage=/acme/acme.json",
+    "--certificatesresolvers.leresolver.acme.httpchallenge.entrypoint=web"
+  ]
+  labels = [
+    {
+      label = "traefik.http.routers.http-catchall.rule"
+      value = "hostregexp(`{host:.+}`)"
+    },
+    {
+      label = "traefik.http.routers.http-catchall.entrypoints"
+      value = "web"
+    },
+    {
+      label = "traefik.http.routers.http-catchall.middlewares"
+      value = "redirect-to-https"
+    },
+    {
+      label = "traefik.http.middlewares.redirect-to-https.redirectscheme.scheme"
+      value = "https"
+    }
+  ]
 }

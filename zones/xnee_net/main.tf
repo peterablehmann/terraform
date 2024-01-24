4eaf5bad6a94c29300fd203489a4ad1ac98ea92d
Peter Lehmann <36541313+peterablehmann@users.noreply.github.com>
2024-01-24 21:35:16 +0000

    Move mns.xnee.net to new server

--- zones/xnee_net/main.tf
+++ zones/xnee_net/main.tf
@@ -98,13 +98,13 @@ resource "hetznerdns_record" "CNAME_fritzbox_xnee_net" {
 resource "hetznerdns_record" "A_mns_xnee_net" {
   zone_id = hetznerdns_zone.xnee_net.id
   name    = "mns"
-  value   = "128.140.47.30"
+  value   = "168.119.172.8"
   type    = "A"
 }
 
 resource "hetznerdns_record" "AAAA_mns_xnee_net" {
   zone_id = hetznerdns_zone.xnee_net.id
   name    = "mns"
-  value   = "2a01:4f8:c0c:5a5::1"
+  value   = "2a01:4f8:1c1e:ad66::1"
   type    = "A"
 }
